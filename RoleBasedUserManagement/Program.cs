0086bb5b1d137d4bb620e09e3815f215fd8d3a27
Mahedi Hasan Niloy <niloy.oct@gmail.com>
2023-10-01 12:17:53 +0600

    add required dependency , models and modify appsetting json file

--- RoleBasedUserManagement/Program.cs
+++ RoleBasedUserManagement/Program.cs
@@ -1,9 +1,19 @@
+using UserManagement.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var _GetConnectionString = builder.Configuration.GetConnectionString("connMSSQL");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(_GetConnectionString));
+
+
+
 
 var app = builder.Build();
 
